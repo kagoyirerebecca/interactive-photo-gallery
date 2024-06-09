c1f0a473cfd562787b1969a8ea55ad62e5942288
kagoyirerebecca <rebeccanw386@gmail.com>
2024-06-09 23:48:35 +0200

    made gallery

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const overlays = document.querySelectorAll('.overlay');
-
-overlays.forEach(overlay => {
-    overlay.addEventListener('click', () => {
-        overlay.classList.toggle('active');
-        const text = overlay.querySelector('.text');
-        text.style.display = text.style.display === 'block' ? 'none' : 'block';
-    });
-});
